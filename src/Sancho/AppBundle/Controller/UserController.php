be4f25f234e7e7dc11187bb26cf969f4558187be
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-06-11 17:19:28 -0500

    init user show action

--- src/Sancho/AppBundle/Controller/UserController.php
+++ src/Sancho/AppBundle/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace Sancho\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
@@ -10,10 +11,29 @@ class UserController extends Controller
 {
     /**
      * @Route("/signup")
+     * @Method("GET")
      * @Template
      */
     public function newAction()
     {
         return array();
+    }
+
+    /**
+     * @Route("users/{id}")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('SanchoAppBundle:User')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find User entity.');
+        }
+
+        return array('entity' => $entity);
     }
 }
